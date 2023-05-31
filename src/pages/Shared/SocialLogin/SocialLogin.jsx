70beeda8781a1ee29689f50587fe836953a269c4
fayezf <mohammadfayezahmed835@gmail.com>
2023-05-31 19:28:56 +0600

    admin user added

--- src/pages/Shared/SocialLogin/SocialLogin.jsx
+++ src/pages/Shared/SocialLogin/SocialLogin.jsx
@@ -1,21 +1,40 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../../providers/AuthProvider';
 import { FaGoogle } from 'react-icons/fa';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const SocialLogin = () => {
     const { googleSignIn } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const from = location.state?.from?.pathname || "/";
 
     const handleGoogle = () => {
         googleSignIn()
             .then(result => {
-                console.log(result.user)
+                const loggedUser = result.user;
+                console.log(loggedUser)
+                const saveUser = { name: loggedUser.displayName, email: loggedUser.email }
+                fetch('http://localhost:5000/users', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(saveUser)
+                })
+                    .then(res => res.json())
+                    .then(() => {
+                        navigate(from, { replace: true });
+                    })
+
             })
             .catch(error => console.log(error))
     }
     return (
-        <div>
-            <div className="divider">OR</div>
-            <div className='flex justify-center items-center border border-gray-600rounded p-1 mb-2 gap-2'>
+        <div className='p-8'>
+            <div className="divider"></div>
+            <div className='flex justify-center items-center border border-gray-500 rounded py-2 gap-2'>
                 <p onClick={handleGoogle} className="btn btn-circle btn-outline text-cyan-700"><FaGoogle></FaGoogle></p>
                 <div>
                     <span className='text-green-900 font-bold'>Google</span>
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, useEffect, useState } from 'react';
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import app from '../firebase/firebase.config';
+import axios from 'axios';
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
@@ -41,7 +42,21 @@ const AuthProvider = ({ children }) => {
         const unSubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
             console.log('current User', currentUser);
-            setLoading(false)
+
+            // get and set token
+            if(currentUser){
+                axios.post('http://localhost:5000/jwt', {email: currentUser.email})
+                .then(data => {
+                    // console.log(data.data.token)
+                    localStorage.setItem('access-token', data.data.token)
+                    setLoading(false)
+                })
+            }
+            else{
+                localStorage.removeItem('access-token')
+            }
+
+            
         });
         return () => {
             return unSubscribe();
@@ -53,6 +68,7 @@ const AuthProvider = ({ children }) => {
         createUser,
         signIn,
         googleSignIn,
+
         logOut,
         updateUserProfile
     }
